fab409cb3fc69cc638f96a68e045dae01b2be811
mahmood.awd <mahmood.awd@bass.com.eg>
2024-04-15 10:15:51 +0200

    Memento-Exercise

---
+++ src/main/java/dev/awd/memento/excercise/CanvasState.java
@@ -0,0 +1,48 @@
+package dev.awd.memento.excercise;
+
+
+/**
+ * A POJO holds state parameters to used within a canvas
+ */
+public class CanvasState {
+    private String content;
+    private String color;
+    private String border;
+
+
+    public CanvasState() {
+        this.content = "";
+        this.color = "";
+        this.border = "";
+    }
+
+    public CanvasState(String content, String color, String border) {
+        this.content = content;
+        this.color = color;
+        this.border = border;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBorder() {
+        return border;
+    }
+
+    public void setBorder(String border) {
+        this.border = border;
+    }
+}
