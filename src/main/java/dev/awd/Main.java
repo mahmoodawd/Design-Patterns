2fa9e73025d04f45ac5c245393722ca54de998fa
mahmood.awd <mahmood.awd@bass.com.eg>
2024-05-12 13:56:44 +0300

    Mediator-FollowUp

--- src/main/java/dev/awd/Main.java
+++ src/main/java/dev/awd/Main.java
@@ -1,18 +1,22 @@
 package dev.awd;
 
-import dev.awd.state.excercise.Task;
-import dev.awd.state.excercise.TaskManagement;
+import dev.awd.mediator.ChatManagement;
+import dev.awd.mediator.ChatMediator;
+import dev.awd.mediator.User;
 
 public class Main {
     public static void main(String[] args) {
-        TaskManagement taskManagement = new TaskManagement(new Task("Study DS"));
+        ChatMediator chatManagement = new ChatManagement();
 
+        User mahmoud = new User("Mahmoud", chatManagement);
+        User ali = new User("Ali", chatManagement);
+        User khaled = new User("Khaled", chatManagement);
 
-        taskManagement.markAsToDo();
-        taskManagement.markAsInProgress();
-        taskManagement.markAsInReview();
-        taskManagement.markAsDone();
-        taskManagement.blockTask();
+        chatManagement.registerUserToGroup(mahmoud, "Friends");
+        chatManagement.registerUserToGroup(ali, "Friends");
+        chatManagement.registerUserToGroup(khaled, "Friends");
+
+        mahmoud.sendGroupMessage("Hello Everyone", "Friends");
 
     }
 }
---
+++ src/main/java/dev/awd/mediator/ChatMediator.java
@@ -0,0 +1,10 @@
+package dev.awd.mediator;
+
+public interface ChatMediator {
+
+    void sendDirectMessage(String message, User fromUser, User toUser);
+
+    void sendGroupMessage(String message, User user, String groupName);
+
+    void registerUserToGroup(User user, String groupName);
+}
